2d9b31a6f271d3574ce0767d66fcb84084116ea2
Onur <onurcoding@gmail.com>
2018-10-20 19:21:53 +0300

    use CQT, not STFT
    CQT, separate train/valid

--- neural.py
+++ neural.py
@@ -9,66 +9,111 @@ from numpy.lib.stride_tricks import as_strided
 import matplotlib.pyplot as plt
 # Reference #9: https://www.kaggle.com/asparago/simple-pitch-detector  
 
-LEN = 1024
-SR = 22050
-FMAX = 11025
-fbins = librosa.fft_frequencies(sr=SR)
-freqs = dict()
-x, y = ([], [])
 model = None
-# ----------
+trainx, trainy = ([], [])
+validx, validy = ([], [])
+testx, testy = ([], [])
+
+BINS = 84
+FREF = librosa.note_to_hz('C1')  # Reference frequency = 32.70 Hz -> C1
+fbins = librosa.cqt_frequencies(BINS, fmin=FREF)
+FMAX = fbins[BINS-1]
+freqs = dict()
+
 STD = 25 # Standard deviation for the probability vector
-FREF = 10  # Reference frequency = 10 Hz
 
-notes = {'Elo':['E2','F2','F#2/Gb2','G2','G#2/Ab2','A2','A#2/Bb2','B2','C3','C#3/Db3','D3','D#3/Eb3','E3'],
-'A':['A2','A#2/Bb2','B2','C3','C#3/Db3','D3','D#3/Eb3','E3','F3','F#3/Gb3','G3','G#3/Ab3','A3'],
-'D':['D3','D#3/Eb3','E3','F3','F#3/Gb3','G3','G#3/Ab3','A3','A#3/Bb3','B3','C4','C#4/Db4','D4'],
-'G':['G3','G#3/Ab3','A3','A#3/Bb3','B3','C4','C#4/Db4','D4','D#4/Eb4','E4','F4','F#4/Gb4','G4'],
-'B':['B3','C4','C#4/Db4','D4','D#4/Eb4','E4','F4','F#4/Gb4','G4','G#4/Ab4','A4','A#4/Bb4','B4'],
-'Ehi':['E4','F4','F#4/Gb4','G4','G#4/Ab4','A4','A#4/Bb4','B4','C5','C#5/Db5','D5','D#5/Eb5','E5']}
-testn = ['A2', 'B3', 'D3', 'E4', 'E2', 'G3', 'A1', 'D2', 'E1', 'G2']
-testf = ['Ac1.mp3', 'Ac2.mp3', 'Ac3.mp3', 'Ac4.mp3', 'Ac5.mp3', 'Ac6.mp3', 'Ba1.mp3', 'Ba2.mp3', 'Ba3.mp3', 'Ba4.mp3']
+# ----------
+# SR = 22050
+# FMAX = 11025
+# LEN = 1024
+# fbins = librosa.fft_frequencies(sr=SR)
+# ----------
 
-def sampleWav(filename, freq):
-    global x, y
+notes = ['Elo', 'A', 'D', 'G', 'B', 'Ehi']
+all_notes = {'Elo':['E2','F2','F#2/Gb2','G2','G#2/Ab2','A2','A#2/Bb2','B2','C3','C#3/Db3','D3','D#3/Eb3','E3'],
+                    'A':['A2','A#2/Bb2','B2','C3','C#3/Db3','D3','D#3/Eb3','E3','F3','F#3/Gb3','G3','G#3/Ab3','A3'],
+            'D':['D3','D#3/Eb3','E3','F3','F#3/Gb3','G3','G#3/Ab3','A3','A#3/Bb3','B3','C4','C#4/Db4','D4'],
+            'G':['G3','G#3/Ab3','A3','A#3/Bb3','B3','C4','C#4/Db4','D4','D#4/Eb4','E4','F4','F#4/Gb4','G4'],
+                    'B':['B3','C4','C#4/Db4','D4','D#4/Eb4','E4','F4','F#4/Gb4','G4','G#4/Ab4','A4','A#4/Bb4','B4'],
+                    'Ehi':['E4','F4','F#4/Gb4','G4','G#4/Ab4','A4','A#4/Bb4','B4','C5','C#5/Db5','D5','D#5/Eb5','E5']}
+
+valid_notes = ['A#2/Bb2', 'C3', 'D#3/Eb3', 'G#3/Ab3', 'C4', 'F#4/Gb4']
+test_notes = ['A2', 'B3', 'D3', 'E4', 'E2', 'G3', 'A1', 'D2', 'E1', 'G2']
+test_files = ['Ac1.mp3', 'Ac2.mp3', 'Ac3.mp3', 'Ac4.mp3', 'Ac5.mp3', 'Ac6.mp3', 'Ba1.mp3', 'Ba2.mp3', 'Ba3.mp3', 'Ba4.mp3']
+#test_notes = ['A2', 'B3', 'D3', 'E4', 'E2', 'G3', 'G2']
+#test_files = ['Ac1.mp3', 'Ac2.mp3', 'Ac3.mp3', 'Ac4.mp3', 'Ac5.mp3', 'Ac6.mp3', 'Ba4.mp3']
+
+def sampleWav(filename, freq, type=0):
+    global trainx, trainy, validx, validy, testx, testy
+    fraction = 2
+    if(filename.startswith('Ac') or filename.startswith('Ba')):
+        fraction = 3
+
     data, sampleRate = librosa.load(filename)
-    D = librosa.stft(data)
-    Spec = librosa.amplitude_to_db(librosa.magphase(D)[0], ref=np.min)
-    
-    #pieces =  Spec.shape[1]
-    #if pieces < 3: return
-    #rand = random.sample(range(0, pieces), pieces) # Pick random frames
-    
-    pV = probVector(freq)
-    temp = Spec[:, 22]
-    x.append(temp)
-    y.append(pV)
-    
-    #for i in range(len(rand)):
-    #   x.append(Spec[:,rand[i]])
-    #    y.append(pV)
+    length = int(len(data)/fraction)
+    D = np.abs(librosa.cqt(data[:length], sr=sampleRate))
+    Spec = np.square(librosa.amplitude_to_db(librosa.magphase(D)[0], ref=np.min).T)
+    frames = len(Spec)
+
+    z = 5
+    if type == 1:
+        z = 10
+
+    rand = random.sample(range(0, frames), int(frames/z)) # Pick random data
+    pV = np.tile(probVector(freq), (len(rand),1))
+
+    #print("%s, total: %d, data_section: %d, frames: %d" % (filename, len(data), length, frames))
+
+    if type == 0:
+        if len(trainx) == 0:
+            trainx = np.array(Spec[rand,:])
+            trainy = pV
+        else:
+            trainx = np.append(trainx, Spec[rand,:], axis=0)
+            trainy = np.append(trainy, pV, axis=0)
+        
+    elif type == 1:
+        if len(validx) == 0:
+            validx = np.array(Spec[rand,:])
+            validy = pV
+            testx = np.array(Spec[rand,:])
+            testy = pV
+        else:
+            validx = np.append(validx, Spec[rand,:], axis=0)
+            validy = np.append(validy, pV, axis=0)
+            testx = np.append(testx, Spec[rand,:], axis=0)
+            testy = np.append(testy, pV, axis=0)
+
+    """elif type == 2:
+        if len(testx) == 0:
+            testx = np.array(Spec[rand,:])
+            testy = pV
+        else:
+            testx = np.append(testx, Spec[rand,:], axis=0)
+            testy = np.append(testy, pV, axis=0)   """     
     return
 
 def freq2Index(freq):
-    return int(round(1024*freq/FMAX))
+    if freq < FREF:
+        return 0
+    elif freq > FMAX:
+        return BINS-1
+    else:
+        return round(math.log2(freq/FREF) * 12)
     
 def index2Freq(index): # Converting the vector index into cents
     return fbins[index]
     
 def probVector(freq): # One-Hot frequency probability vector
-    c = np.zeros(1025)
-    
-    for i in range(1025):
-        if i == 0:
-            c[i] = 0
-        else:
-            dif = 1200*math.log(fbins[i]/freq)/math.log(2) # Difference in cents
-            c[i] = math.exp(dif**2/-2/STD**2)
+    c = np.zeros(BINS)
+
+    for i in range(BINS):
+        dif = 1200*math.log2(fbins[i]/freq) # Difference in cents
+        c[i] = math.exp(dif**2/-2/STD**2)
     return c
 
-def init():
-    global freqs, x, y
-    x, y = ([], [])
+def initData():
+    global freqs
     os.chdir('C:/Users/Onur/Desktop/music-transcription-research')
     with open("source/guitar/freqs.txt") as f:
         for line in f:
@@ -76,21 +121,32 @@ def init():
             freqs[key] = float(val)      
 
     os.chdir("source/guitar")
-    for string in notes: # Dataset 1
+
+    for index, string in enumerate(notes): # Dataset 1
+        print("[%d/%d] %s processing..." % (index+1, len(notes), string))
         for i in range(9):
             filename = "%s%d.wav" % (string, i)
-            note = notes[string][i] # Picking the note
+            note = all_notes[string][i] # Picking the note
             freq = freqs[note] # Picking the frequency value
-            sampleWav(filename, freq)
-    for i, f in enumerate(testf): # Dataset 2
-        freq = freqs[testn[i]]
-        sampleWav(f, freq)
+
+            found = False # true (validation set), false (training set)
+            for j in valid_notes:
+                if j == note:
+                    found = True
+
+            if found:
+                if freq <= freqs[all_notes[string][4]]:
+                    sampleWav(filename, freq, 1)
+            else:
+                sampleWav(filename, freq, 0)
+    
+    print("Processing test files, please wait...")
+    for i, f in enumerate(test_files): # Dataset 2
+        freq = freqs[test_notes[i]]
+        sampleWav(f, freq, 0)
     os.chdir("../..")
 
-    x = np.array(x)
-    y = np.array(y)
-
-def initSinusoids():
+"""def initSinusoids():
     global x, y
     x, y = ([], [])
     N = 1800
@@ -103,7 +159,7 @@ def initSinusoids():
         y.append(probVector(freq[i]))
 
     x = np.array(x)
-    y = np.array(y)
+    y = np.array(y)"""
 
 def modelClear():
     global model
@@ -114,52 +170,24 @@ def modelInit():
 
     if model is None:
         # Sequential model de denenebilir
-        a = Input(shape=(1025,), name='input', dtype='float32')
-        #b = Reshape(target_shape=(1025,), name='input-reshape')(a)
+        a = Input(shape=(BINS,), name='input', dtype='float32')
+        #b = Reshape(target_shape=(BINS,), name='input-reshape')(a)
         
         # Sigmoid yerine relu?
-        b = Dense(1025, activation='sigmoid', name="dense")(a)
-        b = Dropout(0.25, name="dropout")(b)
-        b = Dense(1025, activation='sigmoid', name="classifier")(b)
+        b = Dense(BINS, activation="sigmoid", name="dense")(a)
+        b = Dropout(0.15, name="dropout")(b)
+        b = Dense(BINS, activation="sigmoid", name="dense2")(b)
+        b = Dropout(0.15, name="dropout2")(b)
+        b = Dense(BINS, activation="softmax", name="classifier")(b)
 
         model = Model(inputs=a, outputs=b)
         model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
 
-def oldModelInit():
-    global model
-
-    if model is None:
-        model_capacity = 32
-        layers = [1, 2, 3, 4, 5, 6]
-        filters = [n * model_capacity for n in [32, 4, 4, 4, 8, 16]]
-        widths = [512, 64, 64, 64, 64, 64]
-        strides = [(4, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1)]
-
-        a = Input(shape=(1024,), name='input', dtype='float32')
-        b = Reshape(target_shape=(1024, 1, 1), name='input-reshape')(a)
-
-        for layer, filters, width, strides in zip(layers, filters, widths, strides):
-            b = Conv2D(filters, (width, 1), strides=strides, padding='same',
-                       activation='relu', name="conv%d" % layer)(b)
-            b = BatchNormalization(name="conv%d-BN" % layer)(b)
-            b = MaxPool2D(pool_size=(2, 1), strides=None, padding='valid',
-                          name="conv%d-maxpool" % layer)(b)
-            b = Dropout(0.25, name="conv%d-dropout" % layer)(b)
-
-        b = Permute((2, 1, 3), name="transpose")(b)
-        b = Flatten(name="flatten")(b)
-        b = Dense(360, activation='sigmoid', name="classifier")(b)
-        model = Model(inputs=a, outputs=b)
-        model.compile('adam', 'binary_crossentropy')
-
 def modelSummary():
     model.summary()
 
-def modelFit(e=16, b=16, vs=0.2):
-    l = len(x)
-    lim = l*4//5
-    rand = random.sample(range(0, l), l)
-    model.fit(x=x[rand[:lim]], y=y[rand[:lim]], epochs=e, batch_size=b, validation_data=(x[rand[lim:]], y[rand[lim:]]))
+def modelFit(e=16, b=32):
+    model.fit(x=trainx, y=trainy, epochs=e, batch_size=b, verbose=2, validation_data=(validx, validy))
 
 def modelLoadWeights(filename, sinusoids=True):
     global model
@@ -196,31 +224,33 @@ def testFile(filename, freq):
     plt.plot(v, 'k')
     plt.show()
 
-def test(N=4):
-    rand = random.sample(range(0, len(x)), N) # Pick random data
-    a, b = (x[rand], y[rand])
-
-    a = np.array(a)
-    b = np.array(b)
-    p = model.predict(a)
-
+def test(N=3):
+    if N > len(testx):
+        N = len(testx)-1
+    rand = random.sample(range(0, len(testx)), N) # Pick random data
+    
     plt.figure(figsize=(12, N*6))
     for i in range(N):
+        a, b = (testx[rand[i]], testy[rand[i]])
+        a = np.array([a])
+        b = np.array([b])
+        p = model.predict(a)
+
         plt.subplot(N,2,2*i+1)
-        plt.plot(p[i], 'r')
+        plt.plot(p[0], 'r')
         plt.xlabel('Frequency Bin')
         plt.ylabel('Likelihood')
-        f = p[i].argmax()
+        f = p[0].argmax()
         plt.title('Prediction: %dHz, Index %d' % (index2Freq(f), f))
 
         plt.subplot(N,2,2*i+2)
-        plt.plot(b[i], 'c')
+        plt.plot(b[0], 'c')
         plt.xlabel('Frequency Bin')
-        f = b[i].argmax()
+        f = b[0].argmax()
         plt.title('Truth: %dHz, Index %d' % (index2Freq(f), f))
-    plt.show()   
+    plt.show()
 
-def testSinusoids():
+"""def testSinusoids():
     N = 5
     phi = np.random.uniform(0, 2*math.pi, N)
     freq = np.random.exponential(240, N) + 32.7
@@ -246,4 +276,4 @@ def testSinusoids():
         plt.plot(b[i])
         plt.xlabel('Frequency Bin')
         plt.title('True Probability Vector')
-    plt.show()
+    plt.show()"""
